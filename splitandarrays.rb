f137d646c970a08ef69c6e79d3f439ee05155f62
tanhng <tienanh240599@gmail.com>
2020-03-25 15:50:47 +0700

    split and arrays

---
+++ splitandarrays.rb
@@ -0,0 +1,21 @@
+my_name = " nguyen tien anh"
+
+puts my_name.split
+
+date="17/11/2020"
+
+puts date.split("/")
+
+puts date[0..5]
+
+puts date[0...5]
+
+arrays_family=["kin","bi","Diep"]
+
+puts arrays_family
+
+abc_arrays=["a","b","c","d","e","f","g","h"]
+
+puts abc_arrays[2]
+
+puts abc_arrays[-2]

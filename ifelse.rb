88a9bc1e4704fa9af5d6a94051385895c7666718
tanhng <tienanh240599@gmail.com>
2020-03-25 15:29:49 +0700

    ifelse

---
+++ ifelse.rb
@@ -0,0 +1,9 @@
+your_age = 1000
+if your_age <18
+    puts "you're not old enough"
+elsif your_age<100
+    puts "okay"
+else 
+    puts "who are you?"
+end
+
---
+++ textedit.rb
@@ -0,0 +1,6 @@
+my_name="tanhng"
+puts my_name.upcase
+puts my_name.capitalize
+puts my_name.reverse
+puts my_name.length
+puts my_name[2..5]
